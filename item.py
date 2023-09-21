97cc11be04da97c502e7413141838034d82ed375
Acetorrent <phihoangkhai2811@gmail.com>
2023-09-20 19:44:16 -0500

    Added new features in Recovery Items
    - Adding PP Recovery in Item
    - Adding PP Recovery in Pokemon.py
    
    To DO:
    - Add Skill Class
    - Show add couple other methods for handling pokemon class

--- item.py
+++ item.py
@@ -1,5 +1,5 @@
 class item:
-  status_list = ("Frozen", "Sleep", "Confusion", "Burn", "Poison", "Badly Poison", "Paralyze", "Disabled")
+  
 
 
   class recovery:
@@ -10,7 +10,8 @@ class item:
       self.status_recovery = []
 
     def add_status_recovery(self, value: str):
-      for item in self.status_list:
+      status_list = ("Frozen", "Sleep", "Confusion", "Burn", "Poison", "Badly Poison", "Paralyze", "Disabled")
+      for item in status_list:
         if value is item:
           self.status_recovery.append(value)
           print(f"{self.name}, will now cure {value}")
@@ -19,14 +20,30 @@ class item:
     def use(self, user):
       list_attribute = self.__dict__
 
+
       for key, value in list_attribute.items():
+
+        #HP Recovery
         if key is "HP_recovery" and value > 0:
           user.heal(value)
+
+
+        #PP Recovery Check
         elif key is "PP_recovery" and value > 0:
-          user.heal_pp(value)
+          for item in user.get_skill():
+            print (item + "\n")
+          skill = input(str("\n\nChoose A Skill to recover!\n\n"))
+
+          skill.strip(" ")
+          skill = skill.title()
+
+          for item in user.get_skill():
+            if skill is item:
+              user.heal_pp(skill, value)
+
+        #Status Recovery
         elif key is "status_recovery" and len(value) > 0:
           user.heal_status(value)
 
 
-potion = item.recovery("potion", 10, 5)
 
--- pokemon.py
+++ pokemon.py
@@ -1,39 +1,66 @@
 class pokemon:
   
     possible_nature = ("Adamant", "Bashful", "Brave", "Brave", "Bold", "Calm", "Careful", "Docile", "Gentle", "Hardy", "Hasty", "Impish", "Jolly", "Lax", "Mild", "Modest", "Naive", "Naughty", "Quiet", "Quirky", "Rash", "Relaxed", "Sassy", "Serious", "Timid")
-    
+    possible_xp_groups = ("Fast", "Medium Fast", "Medium Slow", "Slow")
     
     def __init__(self, name: str, total_stat: int, nature: str):
 
+        #Name and Nature of the Pokemon
         self.nature = nature
         self.name = name
-        self.level = 1
-        self.total_stat = total_stat
+        self.ability = None
+        self.gender = None
+        
+        #Skill list of the Pokemons
+        self.learnable_skill = []
         self.skill = []
+        
+        #Initiate the Stats, After and Before IVs, Evs, and Final in combat stat
+        self.total_stat = total_stat
         self.base_stats = {"health" : 0, "attack" : 0, "sp attack" : 0, "defense" : 0, "sp defense" : 0, "speed" : 0}
-        self.EVs = {}
-        self.IVs = {}
-        self.final_stat = {}
+        self.EVs = {"health" : 0, "attack" : 0, "sp attack" : 0, "defense" : 0, "sp defense" : 0, "speed" : 0}
+        self.IVs = {"health" : 0, "attack" : 0, "sp attack" : 0, "defense" : 0, "sp defense" : 0, "speed" : 0}
+
+        #Max Base Stat Value
+        self.final_stat = {"health" : 0, "attack" : 0, "sp attack" : 0, "defense" : 0, "sp defense" : 0, "speed" : 0}
+        #Mutable Base Stat in Battle That will need to be reset after
+        self.mutable_stat = {"health" : 0, "attack" : 0, "sp attack" : 0, "defense" : 0, "sp defense" : 0, "speed" : 0}
+        #Status Inflicted:
+        self.status = None
+
+
+        #Level and XP Calculation
+        self.level = 1
+        self.max_xp = 0
+        self.current_xp = 0
+
+    def get_skill(self):
+        return self.skill
 
     def __repr__(self):
         return ("Lvl. {level} | {name}\n".format(level = self.level, name = (self.name).title()))
 
-    def set_stats(self, **values):
-        """
-        Sample Code
+    def heal(self, amount: int):
+        if (amount > 0 and amount <= (self.final_stat["health"] - self.mutable_stat["health"])):
+            self.mutable_stat["health"] += amount
+            value = amount
+            if ((self.mutable_stat["health"]) > self.final_stat["health"]):
+                value = self.final_stat["health"] - self.mutable_stat["health"]
+                self.mutable_stat["health"] = self.final_stat["health"]
         
-        """
+            print(f" {self.name} healed for {value} HP.")
+            
 
-        total_cost = 0
-        stat_dict = {}
+        else:
+            print(f"{self.name}'s Health is full!")
 
-        #Check if the stats entry exist from the stat dictionary
+    def heal_pp(self, skill, amount: int):
+        #Add in the heal PP Function After you add in the Class Skills
+        pass
 
-        for key, item in values.items():
-            if key in self.base_stats:
-                stat_dict[key] = item
-            else:
-                print (f"Failure to set, {key} does not exist in the list of stats.")
+        
+
+
             
         
 
